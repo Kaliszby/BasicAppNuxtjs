c2d89c69e562ec9f8ec173570f2a9991a9578565
Kaliszby <sumyung17qwq@gmail.com>
2022-02-03 09:21:28 +0700

    validate parameter

--- pages/users/_name/index.vue
+++ pages/users/_name/index.vue
@@ -2,3 +2,12 @@
   <!-- _name คือการสร้างชื่อตัวแปรรับพารามิเตอร์ -->
   <h2>Welcome :{{ $route.params.name }}</h2>
 </template>
+
+<script>
+export default {
+  // validate params โดยใช้ regex ให้รับค่าแค่ตัวเลข
+  validate(data) {
+    return /^\d+$/.test(data.params.name)
+  },
+}
+</script>
